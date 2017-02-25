dbc77dfe4321c4b44ec7d609c3c00bd45f47fc16
laurenceputra <laurenceputra@gmail.com>
2017-02-25 22:37:29 +0800

    finished collaborate sorting

--- collaborate_processor.py
+++ collaborate_processor.py
@@ -2,7 +2,7 @@ import pprint
 
 from openpyxl import load_workbook, Workbook
 
-FILENAME = "Collaborate 3- Participant Sign-Up (Responses).xlsx"
+FILENAME = "collaborate_signups.xlsx"
 FIRST_ROW = 2
 NAME_COL = ["D", "E"]
 SCHOOL_COL = "H"
@@ -10,10 +10,9 @@ EMAIL_COL = "J"
 PHONE_COL = "K"
 CHOICE_COL = "L"
 CONSENT_COL = "P"
+SESSIONS = 3
+MAX_SIZE = 7
 
-"""
-Load the students and return a list of signups and a list
-"""
 def load_students():
     workbook = load_workbook(FILENAME)
     signups = []
@@ -32,7 +31,8 @@ def load_students():
                 "email": sheet[EMAIL_COL + cur_row_str].value,
                 "phone": sheet[PHONE_COL + cur_row_str].value,
                 "choices": [x.strip() for x in sheet[CHOICE_COL + cur_row_str].value.split(",")],
-                "consent": sheet[CONSENT_COL + cur_row_str].value
+                "consent": sheet[CONSENT_COL + cur_row_str].value,
+                "assigned": []
             }
             cur_row += 1
             if sheet[NAME_COL[0] + str(cur_row + 1)].value is None:
@@ -40,15 +40,18 @@ def load_students():
             if new_signup["consent"] != "Yes" or new_signup["email"] in emails:
                 continue
             emails.append(new_signup["email"])
+            to_remove = []
             for choice in new_signup["choices"]:
-                if choice == "Systems" or choice == "Engineering":
-                    new_signup["choices"].remove(choice)
+                if choice == "Systems" or choice == "Engineering" or choice == "Software Development":
+                    to_remove.append(choice)
                     continue
                 if choice == "Technology: Big Data":
-                    new_signup["choices"].remove(choice)
-                    choice = "Technology: Big Data, Systems, Engineering"
+                    to_remove.append(choice)
+                    choice = "Technology: Big Data, Software Development, Systems, Engineering"
                     new_signup["choices"].append(choice)
                     continue
+            for choice in to_remove:
+                new_signup["choices"].remove(choice)
             for choice in new_signup["choices"]:
                 if choice not in choices:
                     choices[choice] = 0
@@ -56,19 +59,55 @@ def load_students():
             signups.append(new_signup)
     return signups, choices
 
-"""
-Filtered demand is a list of classes that have met the min threshold to get in
-and is in order to lowest demand to highest
-"""
-def sort_students(students, filtered_demand):
-    print students
+def filter_demand(demand, cutoff):
+    new_demand = sorted([(k,v) for k,v in demand.iteritems() if v > cutoff], key=lambda(k,v): v)
+    return [x for x, y in new_demand]
+
+def sort_students(signups, filtered_demand):
+    assigned_list = []
+    session_counts = []
+    incomplete_signups = []
+    for i in range(SESSIONS):
+        session_counts.append({})
+    for signup in signups:
+        choice = 0
+        for j in range(SESSIONS):
+            for selection in filtered_demand:
+                if selection not in session_counts[j]:
+                    session_counts[j][selection] = 0
+                if selection in signup["choices"] and session_counts[j][selection] < MAX_SIZE:
+                    signup["session" + str(j + 1)] = selection
+                    signup["assigned"].append(selection)
+                    signup["choices"].remove(selection)
+                    session_counts[j][selection] += 1
+                    choice += 1
+                    break
+        if choice < MAX_SIZE:
+            incomplete_signups.append(signup)
+        else:
+            assigned_list.append(signup)
+    filtered_demand = []
+    for i in range(SESSIONS):
+        filtered_demand.append(filter_demand(session_counts[i], -1))
+    print filtered_demand
+    for signup in incomplete_signups:
+        for j in range(SESSIONS):
+            if ("session" + str(j + 1)) not in signup:
+                for choice in filtered_demand[j]:
+                    if choice not in signup["assigned"] and session_counts[j][choice] < MAX_SIZE:
+                        signup["session" + str(j + 1)] = choice
+                        signup["assigned"].append(choice)
+                        session_counts[j][choice] += 1
+                        break
+        assigned_list.append(signup)
+    pprint.pprint(session_counts)
+    return assigned_list
 
 if __name__ == "__main__":
     students, counts = load_students()
-    counts = sorted(counts.iteritems(), key=lambda (k, v): (v, k))
-    for line in counts:
-        if line[1] > 15:
-            print line[0] + ":"
-
-
+    filtered_demand = filter_demand(counts, 15)
+    assigned_list = sort_students(students, filtered_demand)
+    for signup in assigned_list:
+        if len(signup["assigned"]) < SESSIONS:
+            print signup
 
