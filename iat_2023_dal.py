6b166297784a939670aa17d297d71604a18c3b1c
laurenceputra <laurenceputra@outlook.com>
2024-05-05 21:51:00 +0800

    added code to autofill if students fill less than expected

--- iat_2023_dal.py
+++ iat_2023_dal.py
@@ -1,24 +1,28 @@
 import os
 from pprint import pprint
+import random
+import string
 
 from openpyxl import load_workbook, Workbook
 
+DATA_DIR = "./data/"
 EXPORT_FILENAME = "Class List.xlsx"
 EXPORT_MAINLIST_SHEETNAME = "Main List"
 FIRST_CHOICE_COL = "E"
 FIRST_ROW = 6
 NAME_COL = "B"
 NUM_CLASSES = 10
+OUTPUT_DIR = "./output/"
 SCHOOL_COL = "D"
 SESSIONS = 3
 SHEETNAME = "Reg List - IAT2024"
 TITLE_ROW = "4"
 
 def load_students_tasters():
-    filenames = [filename for filename in os.listdir() if 'xlsx' in filename]
+    filenames = [filename for filename in os.listdir(DATA_DIR) if 'xlsx' in filename]
     students = []
     for filename in filenames:
-        students.extend(load_students_from_excel_tasters(filename))
+        students.extend(load_students_from_excel_tasters(DATA_DIR+filename))
     return students
 
 def load_students_from_excel_tasters(filename):
@@ -28,6 +32,7 @@ def load_students_from_excel_tasters(filename):
     signups = []
     cur_row = FIRST_ROW
     finished = False
+    generated_ids = set()
     for sheetname in workbook.sheetnames:
         sheet = workbook[sheetname]
         while not finished:
@@ -35,9 +40,14 @@ def load_students_from_excel_tasters(filename):
             if sheet[NAME_COL[0] + cur_row_str].value is None:
                 finished = True
                 break
+            new_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            while new_id in generated_ids:
+                new_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            generated_ids.add(new_id)
             new_student = {
                 "name": sheet[NAME_COL + cur_row_str].value.strip(),
                 "school": sheet[SCHOOL_COL + cur_row_str].value.strip(),
+                "id": new_id,
                 "choices": [],
                 "assigned": []
             }
@@ -66,7 +76,7 @@ def export_student_tasters_to_excel(students):
         wb.create_sheet(course)
         write_class_list_to_sheet(wb[course], class_list[course], course)
     
-    wb.save(EXPORT_FILENAME)
+    wb.save(OUTPUT_DIR + EXPORT_FILENAME)
     
 
 def generate_class_lists(students):
@@ -80,23 +90,25 @@ def generate_class_lists(students):
                     "session2": [],
                     "session3": []
                 }
-            class_list[student[cur_session]][cur_session].append((student["name"], student["school"]))
+            class_list[student[cur_session]][cur_session].append((student["name"], student["school"], student["id"]))
     return class_list
 
 def write_to_student_list_to_sheet(sheet, students):
     sheet["A1"] = "Name"
     sheet["B1"] = "School"
-    sheet["C1"] = "Session 1"
-    sheet["D1"] = "Session 2"
-    sheet["E1"] = "Session 3"
+    sheet["C1"] = "ID"
+    sheet["D1"] = "Session 1"
+    sheet["E1"] = "Session 2"
+    sheet["F1"] = "Session 3"
     cur_row = 2
     for student in students:
         cur_row_str = str(cur_row)
         sheet["A" + cur_row_str] = student["name"]
         sheet["B" + cur_row_str] = student["school"]
-        sheet["C" + cur_row_str] = student["session1"]
-        sheet["D" + cur_row_str] = student["session2"]
-        sheet["E" + cur_row_str] = student["session3"]
+        sheet["C" + cur_row_str] = student["id"]
+        sheet["D" + cur_row_str] = student["session1"]
+        sheet["E" + cur_row_str] = student["session2"]
+        sheet["F" + cur_row_str] = student["session3"]
         cur_row += 1
 
 
@@ -107,9 +119,10 @@ def write_class_list_to_sheet(sheet, class_list, course_name):
         cur_session = "session" + str(i+1)
         sheet["A" + str(cur_row)] = "SESSION " + str(i+1)
         cur_row += 1
-        for student, school in class_list[cur_session]:
+        for student, school, id in class_list[cur_session]:
             sheet["A" + str(cur_row)] = student
             sheet["B" + str(cur_row)] = school
+            sheet["C" + str(cur_row)] = id
             cur_row += 1
         cur_row += 1
     
--- iat_taster_processor.py
+++ iat_taster_processor.py
@@ -4,6 +4,8 @@ from pprint import pprint
 import random
 
 MAX = 20
+SESSION_COUNT = 3
+STUDENT_CHOICES = 5
 
 def sort_students(signup_list):
     random.shuffle(signup_list)
@@ -12,7 +14,7 @@ def sort_students(signup_list):
     for student in signup_list:
         class_priority = generate_priority_order(student, classes)
         unassigned = False
-        for i in range(3):
+        for i in range(SESSION_COUNT):
             session_id = "session"+str(i+1)
             assigned = False
             for _class in class_priority:
@@ -51,12 +53,24 @@ def generate_priority_order(student, classes):
     class_priority_scores = []
     class_type_count = len(classes)
     total_selections = sum([classes[_class]["demand"] for _class in classes])
+    to_fill = 0
+    if len(student["choices"]) < STUDENT_CHOICES:
+        to_fill = STUDENT_CHOICES - len(student["choices"])
+        class_signup_counts = []
+        for _class in classes:
+            class_signup_counts.append([_class, classes[_class]["demand"]])
+        class_signup_counts = sorted(class_signup_counts, key=lambda _class: _class[1])
+        #print(class_signup_counts)
+        for i in range(to_fill):
+            student["choices"].append(class_signup_counts[i][0])
+            print(class_signup_counts[i][0])
+            classes[class_signup_counts[i][0]]["demand"] += 1
     for _class in student["choices"]:
         signup_coefficient = (1.0*classes[_class]["session1"]/MAX + 1.0*classes[_class]["session2"]/MAX + 1.0*classes[_class]["session3"]/MAX) / 3
         demand_coefficient = (1.0*classes[_class]["demand"]/total_selections) * class_type_count
         score = signup_coefficient - demand_coefficient
         class_priority_scores.append((_class, score))
-        #print({"name": _class, "s1": signup_coefficient, "s2":demand_coefficient, "s3": score})
+        #print({"name": _class, "s1": signup_coefficient, "s2":demand_coefficient, "s3": score, "cls": classes})
     class_priority_scores = sorted(class_priority_scores, key=lambda score: score[1])
     return [_class[0] for _class in class_priority_scores]
 
